3e82d58af88a66150aa513168cc200fca4f3bcc2
Krystian Stebel <krystians@xsigma.uk>
2021-11-08 17:51:59 +0100

    implement reading WAV, create thread count choice UI

--- filterWAV/Form1.Designer.cs
+++ filterWAV/Form1.Designer.cs
@@ -28,134 +28,263 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea9 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea10 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.optionCpp = new System.Windows.Forms.RadioButton();
+            this.optionAsm = new System.Windows.Forms.RadioButton();
+            this.groupBoxLib = new System.Windows.Forms.GroupBox();
+            this.dialogOpenWAV = new System.Windows.Forms.OpenFileDialog();
+            this.wavForm = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.groupBoxInput = new System.Windows.Forms.GroupBox();
+            this.waveChartLeft = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.waveChartRight = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.runButton = new System.Windows.Forms.Button();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.groupBoxLib.SuspendLayout();
+            this.groupBoxInput.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.waveChartLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.waveChartRight)).BeginInit();
             this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // progressBar1
+            // progressBar
             // 
-            this.progressBar1.Location = new System.Drawing.Point(19, 144);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(590, 23);
-            this.progressBar1.TabIndex = 0;
+            this.progressBar.Location = new System.Drawing.Point(22, 427);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(596, 23);
+            this.progressBar.TabIndex = 0;
+            // 
+            // optionCpp
+            // 
+            this.optionCpp.AutoSize = true;
+            this.optionCpp.Location = new System.Drawing.Point(94, 19);
+            this.optionCpp.Name = "optionCpp";
+            this.optionCpp.Size = new System.Drawing.Size(78, 17);
+            this.optionCpp.TabIndex = 1;
+            this.optionCpp.TabStop = true;
+            this.optionCpp.Text = "Run in C++";
+            this.optionCpp.UseVisualStyleBackColor = true;
+            this.optionCpp.CheckedChanged += new System.EventHandler(this.optionCpp_CheckedChanged);
+            // 
+            // optionAsm
+            // 
+            this.optionAsm.AutoSize = true;
+            this.optionAsm.Location = new System.Drawing.Point(6, 19);
+            this.optionAsm.Name = "optionAsm";
+            this.optionAsm.Size = new System.Drawing.Size(82, 17);
+            this.optionAsm.TabIndex = 2;
+            this.optionAsm.TabStop = true;
+            this.optionAsm.Text = "Run in ASM";
+            this.optionAsm.UseVisualStyleBackColor = true;
+            this.optionAsm.CheckedChanged += new System.EventHandler(this.optionAsm_CheckedChanged);
+            // 
+            // groupBoxLib
+            // 
+            this.groupBoxLib.Controls.Add(this.optionAsm);
+            this.groupBoxLib.Controls.Add(this.optionCpp);
+            this.groupBoxLib.Location = new System.Drawing.Point(19, 68);
+            this.groupBoxLib.Name = "groupBoxLib";
+            this.groupBoxLib.Size = new System.Drawing.Size(196, 46);
+            this.groupBoxLib.TabIndex = 3;
+            this.groupBoxLib.TabStop = false;
+            this.groupBoxLib.Text = "Pick a library";
+            // 
+            // dialogOpenWAV
+            // 
+            this.dialogOpenWAV.FileName = "Select a WAV file";
+            this.dialogOpenWAV.Filter = "WAV files (*.wav)|*.wav";
+            this.dialogOpenWAV.Title = "Open WAV file";
+            // 
+            // wavForm
+            // 
+            this.wavForm.Location = new System.Drawing.Point(6, 19);
+            this.wavForm.Name = "wavForm";
+            this.wavForm.Size = new System.Drawing.Size(503, 20);
+            this.wavForm.TabIndex = 4;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(515, 19);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 5;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // groupBoxInput
+            // 
+            this.groupBoxInput.Controls.Add(this.wavForm);
+            this.groupBoxInput.Controls.Add(this.browseButton);
+            this.groupBoxInput.Location = new System.Drawing.Point(19, 4);
+            this.groupBoxInput.Name = "groupBoxInput";
+            this.groupBoxInput.Size = new System.Drawing.Size(596, 58);
+            this.groupBoxInput.TabIndex = 6;
+            this.groupBoxInput.TabStop = false;
+            this.groupBoxInput.Text = "Pick a WAV file";
+            this.groupBoxInput.Enter += new System.EventHandler(this.groupBoxInput_Enter);
+            // 
+            // waveChartLeft
+            // 
+            chartArea9.Name = "ChartArea1";
+            this.waveChartLeft.ChartAreas.Add(chartArea9);
+            this.waveChartLeft.Location = new System.Drawing.Point(19, 120);
+            this.waveChartLeft.Name = "waveChartLeft";
+            series9.ChartArea = "ChartArea1";
+            series9.Name = "Series1";
+            this.waveChartLeft.Series.Add(series9);
+            this.waveChartLeft.Size = new System.Drawing.Size(596, 132);
+            this.waveChartLeft.TabIndex = 7;
+            this.waveChartLeft.Text = "chart1";
+            // 
+            // waveChartRight
+            // 
+            chartArea10.Name = "ChartArea1";
+            this.waveChartRight.ChartAreas.Add(chartArea10);
+            this.waveChartRight.Location = new System.Drawing.Point(19, 258);
+            this.waveChartRight.Name = "waveChartRight";
+            series10.ChartArea = "ChartArea1";
+            series10.Name = "Series1";
+            this.waveChartRight.Series.Add(series10);
+            this.waveChartRight.Size = new System.Drawing.Size(596, 132);
+            this.waveChartRight.TabIndex = 8;
+            this.waveChartRight.Text = "chart1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.runButton);
+            this.groupBox1.Location = new System.Drawing.Point(221, 68);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(397, 46);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Number of threads";
+            // 
+            // runButton
+            // 
+            this.runButton.Location = new System.Drawing.Point(313, 13);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(75, 23);
+            this.runButton.TabIndex = 4;
+            this.runButton.Text = "Run";
+            this.runButton.UseVisualStyleBackColor = true;
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(94, 19);
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(78, 17);
-            this.radioButton1.TabIndex = 1;
+            this.radioButton1.Size = new System.Drawing.Size(31, 17);
+            this.radioButton1.TabIndex = 5;
             this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Run in C++";
+            this.radioButton1.Text = "1";
             this.radioButton1.UseVisualStyleBackColor = true;
             this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(6, 19);
+            this.radioButton2.Location = new System.Drawing.Point(43, 19);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(82, 17);
-            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Size = new System.Drawing.Size(31, 17);
+            this.radioButton2.TabIndex = 6;
             this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Run in ASM";
+            this.radioButton2.Text = "2";
             this.radioButton2.UseVisualStyleBackColor = true;
             this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
-            // groupBox1
+            // radioButton3
             // 
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.radioButton2);
-            this.groupBox1.Controls.Add(this.radioButton1);
-            this.groupBox1.Location = new System.Drawing.Point(19, 68);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(596, 46);
-            this.groupBox1.TabIndex = 3;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Pick a library";
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(80, 19);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(31, 17);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "4";
+            this.radioButton3.UseVisualStyleBackColor = true;
             // 
-            // openFileDialog1
+            // radioButton4
             // 
-            this.openFileDialog1.FileName = "openFileDialog1";
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(117, 19);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(31, 17);
+            this.radioButton4.TabIndex = 8;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "8";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
             // 
-            // textBox1
+            // radioButton5
             // 
-            this.textBox1.Location = new System.Drawing.Point(6, 19);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(503, 20);
-            this.textBox1.TabIndex = 4;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(515, 19);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Browse";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.textBox1);
-            this.groupBox2.Controls.Add(this.button1);
-            this.groupBox2.Location = new System.Drawing.Point(19, 4);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(596, 58);
-            this.groupBox2.TabIndex = 6;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Pick a WAV file";
-            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(515, 17);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Run";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(154, 19);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(37, 17);
+            this.radioButton5.TabIndex = 9;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "16";
+            this.radioButton5.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(627, 188);
-            this.Controls.Add(this.groupBox2);
+            this.ClientSize = new System.Drawing.Size(630, 462);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.waveChartRight);
+            this.Controls.Add(this.waveChartLeft);
+            this.Controls.Add(this.groupBoxInput);
+            this.Controls.Add(this.groupBoxLib);
+            this.Controls.Add(this.progressBar);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBoxLib.ResumeLayout(false);
+            this.groupBoxLib.PerformLayout();
+            this.groupBoxInput.ResumeLayout(false);
+            this.groupBoxInput.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.waveChartLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.waveChartRight)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.RadioButton optionCpp;
+        private System.Windows.Forms.RadioButton optionAsm;
+        private System.Windows.Forms.GroupBox groupBoxLib;
+        private System.Windows.Forms.OpenFileDialog dialogOpenWAV;
+        private System.Windows.Forms.TextBox wavForm;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.GroupBox groupBoxInput;
+        private System.Windows.Forms.DataVisualization.Charting.Chart waveChartLeft;
+        private System.Windows.Forms.DataVisualization.Charting.Chart waveChartRight;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton5;
     }
 }
 
--- filterWAV/Form1.cs
+++ filterWAV/Form1.cs
@@ -3,19 +3,222 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace filterWAV
 {
+    
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            waveChartLeft.Series.Clear();
+            waveChartRight.Series.Clear();
+        }
+
+        
+        private static double bytesToDouble(byte firstByte, byte secondByte)
+        {
+          
+            short s = (short)((secondByte << 8) | firstByte);
+            return s / 32768.0;
+        }
+
+      
+        public void openWav(string filename, out double[] left, out double[] right)
+        {
+            byte[] wav = File.ReadAllBytes(filename);
+
+            char[] rawRIFF = new char[4]{ (char)wav[0], (char)wav[1], (char)wav[2], (char)wav[3] };
+            string riff = new string(rawRIFF);
+
+            int fileSize = BitConverter.ToInt32(wav, 4);
+
+            char[] rawWAVE = new char[4] { (char)wav[8], (char)wav[9], (char)wav[10], (char)wav[11] };
+            string wave = new string(rawWAVE);
+            
+            byte[] fmt = new byte[4]{ wav[12], wav[13], wav[14], wav[15]};
+
+            byte[] formatByteSize = new byte[4] { wav[16], wav[17], wav[18], wav[19] };
+
+            int channels = BitConverter.ToInt16(wav,22);    
+            
+            int sampleRate = BitConverter.ToInt32(wav, 24);
+
+            int bytesPerSecond = BitConverter.ToInt32(wav, 28);
+
+            int bytesPerSample = BitConverter.ToInt16(wav, 32);
+
+            int bitsPerSample = BitConverter.ToInt16(wav, 34);
+
+            char[] rawData = new char[4] { (char)wav[36], (char)wav[37], (char)wav[38], (char)wav[39] };
+
+            string data = new string(rawData);
+
+            int dataSize = BitConverter.ToInt32(wav, 40);
+
+            string msg = "Format: " + riff + "\n File size: " + fileSize + "\n File type: " + wave + "\n Sample Rate: "  + sampleRate + "\n Number of channels: " + channels 
+                + "\n Bytes Per Second: " + bytesPerSecond + "\n Bytes per Sample: " + bytesPerSample + "\n Bits per Sample: " 
+                + bitsPerSample + "\n Data: " + data + "\n Data size: " + dataSize;
+
+            MessageBox.Show(msg);
+           
+            
+            int pos = 44;
+           
+            int samples = dataSize / bytesPerSample;
+      
+
+            
+            left = new double[samples];
+            if (channels == 2) right = new double[samples];
+            else right = null; // if mono then right array is null
+
+            int i = 0;
+           while (pos < samples)
+            {
+               
+
+
+
+                left[i] = bytesToDouble(wav[pos], wav[pos + 1]);
+                pos += 2;
+                if (channels == 2)
+                {
+                    right[i] = bytesToDouble(wav[pos], wav[pos + 1]);
+                    pos += 2;
+                }
+                i++;
+            }
+        }
+
+        private void optionCpp_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void optionAsm_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private unsafe void browseButton_Click(object sender, EventArgs e)
+        {
+            if (dialogOpenWAV.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var filePath = dialogOpenWAV.FileName;
+                   
+                     double[] left = new double[] { };
+                      double[] right = new double[] { };
+                      this.openWav(filePath, out left, out right);
+
+                    waveChartLeft.Series.Clear();
+                    waveChartRight.Series.Clear();
+                    Series seriesL = waveChartLeft.Series.Add("WaveLeft");
+                    Series seriesR = waveChartRight.Series.Add("WaveRight");
+
+                    seriesL.ChartType = SeriesChartType.Spline;
+                    seriesR.ChartType = SeriesChartType.Spline;
+                    int samplesPerPixel = left.Length / (2 * (int)waveChartLeft.ChartAreas[0].Position.Width);  
+                    int step = 750;
+                    for(int i = 0; i< left.Length - step; i+=step)
+                    {
+                        double leftLow = left[i];
+                        double leftHigh = left[i];
+                        double leftLowX = i;
+                        double leftHighX = i;
+
+                        double rightLow = left[i];
+                        double rightHigh = left[i];
+                        double rightLowX = i;
+                        double rightHighX = i;
+
+                        for (int j = 0; j < step; j++)
+                        {
+                            
+                            if (left[i + j] < leftLow)
+                            {
+                                leftLow = left[i + j];
+                                leftLowX = i + j;
+                            }
+                            if (left[i + j] > leftHigh)
+                            {
+                                leftHigh = left[i + j];
+                                leftHighX = i + j;
+                            }
+                            if (right[i + j] < rightLow)
+                            {
+                                rightLow = right[i + j];
+                                rightLowX = i + j;
+                            }
+                            if (right[i + j] > rightHigh)
+                            {
+                                rightHigh = right[i + j];
+                                rightHighX = i + j;
+                            }
+
+                        }
+                        
+                          seriesL.Points.AddXY(leftLowX, leftLow);
+                          seriesL.Points.AddXY(leftHighX, leftHigh);
+
+                          seriesR.Points.AddXY(rightLowX, rightLow);
+                          seriesR.Points.AddXY(rightHighX, rightHigh);
+
+
+                    }
+                  //  waveChart.ChartAreas[0].AxisX.Maximum = 1900;
+
+                    string msg = "Array is: " + samplesPerPixel;
+                     MessageBox.Show(msg); 
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+    
+
+        private void groupBoxInput_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            interfaceC cppInterface = new interfaceC();
+            interfaceASM asmInterface = new interfaceASM();
+            string result = "";
+            double x = 5.25;
+            double y = 2.5;
+            if (optionCpp.Checked)
+            {
+                result += "Cpp picked. Result: ";
+                double additionResult = cppInterface.callCFunction(x, y);
+                result += additionResult;
+
+
+
+            }
+            else if (optionAsm.Checked)
+            {
+                result += "ASM picked. Result: ";
+                double substractionResult = asmInterface.callAsmFunction(x, y);
+                result += substractionResult;
+            }
+            MessageBox.Show(result);
         }
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
@@ -28,39 +231,41 @@ namespace filterWAV
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
         {
+
+        }
+
+        private void handleThreads(int numberOfThreads, int sampleCount)
+        {
+            int size = sampleCount;
            
-        }
+           
 
-        private void groupBox2_Enter(object sender, EventArgs e)
-        {
+            
+            float sectionSizeFloat = size / numberOfThreads;
+            uint sectionSize = (uint)Math.Ceiling(sectionSizeFloat);
+            uint lowerBoundry;
+            uint upperBoundry;
 
-        }
+           
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            interfaceC cppInterface = new interfaceC();
-            interfaceASM asmInterface = new interfaceASM();
-            string result = "";
-            double x = 5.25;
-            double y = 2.5;
-            if (radioButton1.Checked)
+            for (int i = 0; i < numberOfThreads; i++)
             {
-                result += "Cpp picked. Result: ";
-                double additionResult = cppInterface.callCFunction(x, y);
-                result += additionResult;
-
-
-
+               // divide sample table(s) for threads
             }
-            else if (radioButton2.Checked)
+
+
+
+
+
+            for (int i = 0; i < numberOfThreads; i++)
             {
-                result += "ASM picked. Result: ";
-                double substractionResult = asmInterface.callAsmFunction(x, y);
-                result += substractionResult;
+               //join threds
             }
-            MessageBox.Show(result);
+
         }
     }
+
+
 }
